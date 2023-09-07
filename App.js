78fea0d53f4b4ae5d32b5719b0a2a64ab65abeb2
RonaldThomasAlex <ronaldthomas094@gmail.com>
2023-09-07 23:16:42 +0530

    logout

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 
+import IconButton from "./components/ui/IconButton";
+
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
@@ -28,6 +30,8 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -36,7 +40,20 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 }
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="exit"
+              color={tintColor}
+              size={24}
+              onPress={authCtx.logout}
+            />
+          )
+        }}
+      />
     </Stack.Navigator>
   );
 }
